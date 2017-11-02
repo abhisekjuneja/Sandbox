4cd9e41679292ec8a20359f9273646f695333a2e
abhisekjuneja <abhisekjuneja@gmail.com>
2017-11-02 23:25:52 +0530

    favicon Added

--- Clinicea-Project/Work-A-Holic/scripts/app.js
+++ Clinicea-Project/Work-A-Holic/scripts/app.js
@@ -15,3 +15,53 @@ function trackLocalStorageForEmptyData() {
         archivedPageBody.innerHTML = '<b><br /><i class="fa fa-clone fa-4x" aria-hidden="true"></i><br /><br />Archived Task List is Empty!<br /><br /> Click on the <i class="icon-plus-sign"></i>  icon to Add a New Task</b>';
     }
 }
+
+let search = document.getElementById('search-option');
+search.addEventListener('click', function () {
+    showDialog({
+        id: 'dialog-id',
+        title: 'Title',
+        text: 'Text',
+        negative: {
+            id: 'cancel-button',
+            title: 'Cancel',
+            onClick: function () { }
+        },
+        positive: {
+            id: 'ok-button',
+            title: 'OK',
+            onClick: function () { }
+        },
+        cancelable: true,
+        contentStyle: { 'max-width': '500px' },
+        onLoaded: function () { },
+        onHidden: function () { }
+    });
+});
+
+window.onload = function () {
+    let search = document.getElementById('search-option');
+    console.log(search);
+    // .addEventListener('click', function() {
+    //     console.log('c');
+    //     showDialog({
+    //         id: 'dialog-id',
+    //         title: 'Title',
+    //         text: 'Text',
+    //         negative: {
+    //             id: 'cancel-button',
+    //             title: 'Cancel',
+    //             onClick: function() { }
+    //         },
+    //         positive: {
+    //             id: 'ok-button',
+    //             title: 'OK',
+    //             onClick: function() { }
+    //         },
+    //         cancelable: true,
+    //         contentStyle: {'max-width': '500px'},
+    //         onLoaded: function() { },
+    //         onHidden: function() { }
+    //     });
+    // });
+}
